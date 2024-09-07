f84850b39e3843176fd41b4d571b276bbc73f958
html5syt <html5syt@163.com>
2024-09-07 11:29:49 +0800

    多色准备

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import flet as ft
 
 from flet_contrib.color_picker import ColorPicker
 
+
 def main(page: ft.Page):
     # global nick
 
@@ -9,8 +10,46 @@ def main(page: ft.Page):
     # 导航栏
     def navigationTo(e: ft.ControlEvent):
         nonlocal navigation
-        navigation = e.data
-        # print(navigation)
+        # 防止重复点击
+        if navigation != e.data:
+            navigation = e.data
+            delList = [
+                nick,
+                divider,
+                colors,
+                parameter,
+                divider1,
+                result,
+                multi_Btn,
+            ]
+            n = 0
+            for i in delList:
+                n += 1
+                try:
+                    page.controls.remove(i)
+                    page.update()
+                except:
+                    print(f"error:{i},on{n}")
+            if int(navigation) == 0:
+                page.add(
+                    nick,
+                    divider,
+                    colors,
+                    parameter,
+                    divider1,
+                    result,
+                )
+            elif int(navigation) == 1:
+                page.add(
+                    # nick,
+                    # divider,
+                    # colors,
+                    # parameter,
+                    # divider1,
+                    multi_Btn,
+                    result,
+                )
+            # print(navigation)
 
     page.adaptive = True  # 自适应
     page.fonts = {
@@ -205,69 +244,79 @@ def main(page: ft.Page):
             )
         )
         page.update()
-    sizeN=None
+
+    sizeN = None
+
     # 颜色处理
-    def colorProssing(e:ft.ControlEvent):
-        nonlocal navigation, copytext, size,colors,color_picker_ctrl,nick,sizeN
+    def colorProssing(e: ft.ControlEvent):
+        nonlocal navigation, copytext, size, colors, color_picker_ctrl, nick, sizeN
         # colors.value = colors.value
         # print("navigation:",navigation)
         # 单色部分
         # print("sizeN55556",sizeN)
-        preview.content.spans[0].style=ft.TextStyle()
+        preview.content.spans[0].style = ft.TextStyle()
         if int(navigation) == 0:
             # print(e.control)
-            resultTmp = nick[0].value
-            preview.content.spans[0].text = nick[0].value
+            resultTmp = nick.value
+            preview.content.spans[0].text = nick.value
             # preview.content.spans[0].style=None
             if bold.value:
                 resultTmp = f"<b>{resultTmp}</b>"
                 # preview.content.spans[0].text=resultTmp
-                preview.content.spans[0].style.weight=ft.FontWeight.BOLD
+                preview.content.spans[0].style.weight = ft.FontWeight.BOLD
             if itatic.value:
                 resultTmp = f"<i>{resultTmp}</i>"
-                preview.content.spans[0].style.italic=True
-            if sizeN!=None:
+                preview.content.spans[0].style.italic = True
+            if sizeN != None:
                 # print("sizeN:",sizeN)
                 resultTmp = f"<size={size.value}>{resultTmp}</size>"
-                preview.content.spans[0].style.size=size.value
+                preview.content.spans[0].style.size = size.value
             try:
-                if ("字号" == e.control.label and size.value != "0"):
+                if "字号" == e.control.label and size.value != "0":
                     try:
-                        if float(size.value) !=0:
+                        if float(size.value) != 0:
                             size.error_text = ""
                             resultTmp = f"<size={size.value}>{resultTmp}</size>"
-                            preview.content.spans[0].style.size=size.value
-                            sizeN= size.value
+                            preview.content.spans[0].style.size = size.value
+                            sizeN = size.value
                     except:
                         size.error_text = "请输入正确的数字！"
             except:
                 if size.value != "0" or size.value != None:
                     # print("size:", type(float(size.value)))
                     try:
-                        if float(size.value) !=0:
+                        if float(size.value) != 0:
                             size.error_text = ""
                             resultTmp = f"<size={size.value}>{resultTmp}</size>"
-                            preview.content.spans[0].style.size=size.value
-                            sizeN= size.value
+                            preview.content.spans[0].style.size = size.value
+                            sizeN = size.value
                     except:
                         size.error_text = "请输入正确的数字！"
-                        sizeN=None
-            for key,value in {"红色":"red", "橙色":"orange", "黄色":"yellow", "绿色":"green", "蓝色":"blue","靛色":"#4b0082", "紫色":"purple"}.items():
+                        sizeN = None
+            for key, value in {
+                "红色": "red",
+                "橙色": "orange",
+                "黄色": "yellow",
+                "绿色": "green",
+                "蓝色": "blue",
+                "靛色": "#4b0082",
+                "紫色": "purple",
+            }.items():
                 # print(colors.value)
                 if colors.value == key:
                     resultTmp = f"<color={value}>{resultTmp}</color>"
-                    preview.content.spans[0].style.color=value
+                    preview.content.spans[0].style.color = value
             if colors.value == "自选颜色":
-                resultTmp = f"<color={color_picker_ctrl.color}>{resultTmp}</color>" 
-                preview.content.spans[0].style.color=color_picker_ctrl.color
+                resultTmp = f"<color={color_picker_ctrl.color}>{resultTmp}</color>"
+                preview.content.spans[0].style.color = color_picker_ctrl.color
             copytext.value = f"{resultTmp}\nGearnerated by PhiName"
-        if nick[0].value == "":
+        if nick.value == "":
             resultTmp = ""
             copytext.value = ""
             preview.content.spans[0].text = "—暂无预览—"
         page.update()
 
-    color=""
+    color = ""
 
     # 颜色列表更改事件
     def color_change(e):
@@ -323,28 +372,24 @@ def main(page: ft.Page):
                 copytext.expand = True
                 page.add(result)
                 # page.update()
-            colorProssing(e,)
+            colorProssing(
+                e,
+            )
 
     # 组件
-    # 公用
+    # 公用区
 
-    nick = (
-        ft.TextField(
-            adaptive=True,
-            label="请输入昵称",
-            hint_text="请输入昵称...",
-            on_change=colorProssing,
-            on_submit=colorProssing,
-        ),
-    )
+    divider = ft.Divider()
+    divider1 = ft.Divider()
 
     bold = ft.Switch(label="加粗 ", value=False, adaptive=True, on_change=colorProssing)
     itatic = ft.Switch(
         label="斜体", value=False, adaptive=True, on_change=colorProssing
     )
+
     def sizeT(e):
         nonlocal sizeN
-        sizeN=None
+        sizeN = None
         colorProssing(e)
 
     size = ft.TextField(
@@ -375,7 +420,7 @@ def main(page: ft.Page):
             color=ft.colors.WHITE,
             font_family="text",
             spans=[
-                ft.TextSpan(text="—暂无预览—",style=ft.TextStyle()),
+                ft.TextSpan(text="—暂无预览—", style=ft.TextStyle()),
             ],
         ),
         bgcolor=ft.colors.BLACK,
@@ -393,19 +438,23 @@ def main(page: ft.Page):
         ]
     )
 
-    # 单色/多色
+    # 单色/多色公用
+    parameter = ft.Row(
+        [bold, itatic, ft.VerticalDivider(), size],
+        alignment=ft.MainAxisAlignment.SPACE_AROUND,
+    )
     colors = ft.Dropdown(
         label="颜色",
         hint_text="选择您喜欢的颜色...",
         on_change=color_change,
         options=[
-            ft.dropdown.Option("红色",on_click=colorProssing),
-            ft.dropdown.Option("橙色",on_click=colorProssing),
-            ft.dropdown.Option("黄色",on_click=colorProssing),
-            ft.dropdown.Option("绿色",on_click=colorProssing),
-            ft.dropdown.Option("蓝色",on_click=colorProssing),
-            ft.dropdown.Option("靛色",on_click=colorProssing),
-            ft.dropdown.Option("紫色",on_click=colorProssing),
+            ft.dropdown.Option("红色", on_click=colorProssing),
+            ft.dropdown.Option("橙色", on_click=colorProssing),
+            ft.dropdown.Option("黄色", on_click=colorProssing),
+            ft.dropdown.Option("绿色", on_click=colorProssing),
+            ft.dropdown.Option("蓝色", on_click=colorProssing),
+            ft.dropdown.Option("靛色", on_click=colorProssing),
+            ft.dropdown.Option("紫色", on_click=colorProssing),
             ft.dropdown.Option("自选颜色"),
         ],
         # on_click=colorProssing,
@@ -426,15 +475,28 @@ def main(page: ft.Page):
 
     # CP = ft.Row([color_picker], alignment=ft.MainAxisAlignment.SPACE_AROUND, wrap=True,scroll=True)
 
+    # 单色/渐变公用
+    nick = ft.TextField(
+        adaptive=True,
+        label="请输入昵称",
+        hint_text="请输入昵称...",
+        on_change=colorProssing,
+        on_submit=colorProssing,
+    )
+
+    #独占区
+    #多色
+    multi_Btn= ft.Row(
+        [ft.FilledButton("开启多选",adaptive=True,expand=True),ft.FilledButton("自动填充",tooltip="Tip：长按可平均分割~",adaptive=True,expand=True)]
+    )
+
+    #初始化组件
     page.add(
-        nick[0],
-        ft.Divider(),
+        nick,
+        divider,
         colors,
-        ft.Row(
-            [bold, itatic, ft.VerticalDivider(), size],
-            alignment=ft.MainAxisAlignment.SPACE_AROUND,
-        ),
-        ft.Divider(),
+        parameter,
+        divider1,
         result,
     )
 
